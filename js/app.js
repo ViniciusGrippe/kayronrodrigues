502dae15eafd5d53ad58ae8cb5eaaf64c0a970a3
Vinícius Grippe <oviniciusgrippe@gmail.com>
2020-07-01 19:21:22 -0400

    v 1.2

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-$(function () {
-  $("a.link_arrow").mouseover(function() {
-    $(".arrow").animate({
-      marginTop: "20px"},
-      500, function(){
-      });
-    });
-  $("a.link_arrow").mouseout(function() {
-      $(".arrow").animate({
-        marginTop: "0px"},
-        500, function(){
-        });
-      });
+$(function(){
+  $('div.see_more').click(function(){
+    if($("div.hidden").is(':hidden')){
+      $("div.hidden").slideDown(500);
+      $('div.see_more span').html('recolher');
+      $('div.see_more').animate({
+        marginTop: "-35px"
+      }, 500);
+    }else{
+      $("div.hidden").slideUp(500);
+      $('div.see_more span').html('veja outras áreas');
+      $('div.see_more').animate({
+        marginTop: "0px"
+      }, 500);
+    }
+  });
 });
+
+// $(function () {
+//   $("a.link_arrow").mouseover(function() {
+//     $(".arrow").animate({
+//       marginTop: "20px"},
+//       500, function(){
+//       });
+//     });
+//   $("a.link_arrow").mouseout(function() {
+//       $(".arrow").animate({
+//         marginTop: "0px"},
+//         500, function(){
+//         });
+//       });
+// });
 
 $(function () {
 
